fc1a1e3d2c43c2c5feb643c44121b2c19baf1874
Anton_Taralo <taraloanton482@yandex.ru>
2021-09-23 23:06:55 +0300

    доработки для доп задания

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 
+#include <math.h>
+
 void Trans(unsigned a) {
     if (a) {
         Trans(a >> 1);
@@ -9,10 +11,18 @@ void Trans(unsigned a) {
 int main() {
     long num;
     scanf("%li", & num);
-    if (num < 0) {
-        num *= -1;
-        printf("-");
+    if (num > pow(2, (8 * sizeof(unsigned int))) - 1) { //максимальное число которое может принять функция
+        printf("Слишком большое число");
+        return 1;
     }
+    if (num == 0) {
+        printf("0");
+    }
+    if (num)
+        if (num < 0) {
+            num *= -1;
+            printf("-");
+        }
     Trans(num);
     return 0;
 }
